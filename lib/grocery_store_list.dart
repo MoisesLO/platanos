57958c0f4a896e33e9428933f7faf22506059ddd
moises <moises@moisess-Mac-Pro.local>
2021-03-06 01:24:29 -0500

    edit

--- lib/grocery_store_list.dart
+++ lib/grocery_store_list.dart
@@ -6,7 +6,14 @@ class GroceryStoreList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = GroceryProvider.of(context).bloc;
-    return StaggeredDualView()
+    return StaggeredDualView(
+      itemBuilder: (context, index) {
+        return MealItem(
+          meal: meals[index]
+        );
+      },
+        itemCount: meals.length,
+    );
     /*
     return ListView.builder(
       padding: EdgeInsets.only(top: cartBarHeight),
@@ -20,5 +27,47 @@ class GroceryStoreList extends StatelessWidget {
       },
     );
     */
+  }
+}
+
+class MealItem extends StatelessWidget {
+  const MealItem({Key key, this.meal}) : super(key: key);
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+class StaggeredDualView extends StatelessWidget {
+  const StaggeredDualView(
+      {
+        Key key,
+        @required this.itemBuilder,
+        @required this.itemCount,
+        this.spacing = 0.0,
+        this.aspectRatio = 0.5
+      })
+      : super(key: key);
+
+  final IndexedWidgetBuilder itemBuilder;
+  final int itemCount;
+  final double spacing;
+  final double aspectRatio;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: aspectRatio,
+        crossAxisSpacing: spacing,
+        mainAxisSpacing: spacing
+      ),
+      itemBuilder: itemBuilder,
+    );
   }
 }
---
+++ lib/meal.dart
@@ -0,0 +1,14 @@
+class Meal {
+  const Meal({this.name, this.weight, this.image});
+  final String name;
+  final String weight;
+  final String image;
+}
+
+const meals = <Meal>[
+  Meal(
+    name: 'dsad',
+    weight: 'dasda',
+    image: 'dsada'
+  )
+];
